4264a09882ed3c6ccaadbef5dfbfd10fcc1a1aa4
unknown <max.muller@stonybrook.edu>
2023-09-15 12:58:29 -0400

    Worked on form

--- src/index.js
+++ src/index.js
@@ -2,4 +2,30 @@ import Model from './model.js';
 
 window.onload = function() {
   // fill me with relevant code
+  const btn = document.getElementById('submit');
+
+  btn.addEventListener('click', () => {
+    const form = document.getElementById('form');
+    const container = document.getElementById('contanier')
+
+    
+  if (form.style.display === 'none') {
+    // 👇️ this SHOWS the form
+    form.style.display = 'block';
+   
+  } else {
+    // 👇️ this HIDES the form
+    form.style.display = 'none';
+    
+  }
+});
+
+
+
+
+
+
+
+
+
 };
