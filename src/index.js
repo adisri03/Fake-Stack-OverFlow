2e7a2e84218dd5c9f42d8207b457facf4e8abb0d
unknown <max.muller@stonybrook.edu>
2023-09-18 10:37:03 -0400

    worked on hiding/displaying

--- src/index.js
+++ src/index.js
@@ -3,24 +3,41 @@ import Model from './model.js';
 window.onload = function() {
   // fill me with relevant code
   const btn = document.getElementById('submit');
+  const formBtn = document.getElementById('post');
 
   btn.addEventListener('click', () => {
     const form = document.getElementById('form');
     const container = document.getElementById('top');
 
     
-  if (form.style.display === 'none') {
-    // 👇️ this SHOWS the form
-    form.style.display = 'block';
-    container.style.display = 'none';
-   
-  } else {
-    // 👇️ this HIDES the form
-    form.style.display = 'none';
-    container.style.display = 'block';
+    if (form.style.display === 'none') {
+      // 👇️ this SHOWS the form
+      form.style.display = 'block';
+      container.style.display = 'none';
     
-  }
+    } else {
+      // 👇️ this HIDES the form
+      form.style.display = 'none';
+      container.style.display = 'block';
+      
+    }
 });
+
+  formBtn.addEventListener('click', () => {
+    const form = document.getElementById('form');
+
+    if (form.style.display === 'none') {
+      // 👇️ this SHOWS the form
+      form.style.display = 'block';
+      container.style.display = 'none';
+     
+    } else {
+      // 👇️ this HIDES the form
+      form.style.display = 'none';
+      container.style.display = 'block';
+      
+    }
+  })
 
 
 
