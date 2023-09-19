f290f7293f2d5c8f11c04bbd92e3b4d0fe4fa592
Max Muller <113369063+maxmuller00@users.noreply.github.com>
2023-09-19 17:13:02 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -87,36 +87,6 @@ window.onload = function() {
     }
 });
 
-  formBtn.addEventListener('click', () => {
-    const title = document.getElementById('questionTitle');
-    const text = document.getElementById('questionText');
-    const tag = document.getElementById('tagText');
-    const name = document.getElementById('userName');
-
-    const titleError = document.getElementById('titleError');
-    const textError = document.getElementById('textError');
-    const tagError = document.getElementById('tagError');
-    const nameError = document.getElementById('nameError');
-
-    const text1 = title.value;
-    const text2 = text.value;
-    const text3 = tag.value;
-    const text4 = name.value
-
-    if (text1.length == 0 || text1.length > 100) {
-    // Display a message indicating the character count is valid
-      titleError.style.display="block";
-    }else if(text2.length == 0){
-      textError.style.display="block";
-    }else if(text3.length == 0){
-     tagError.style.display="block";
-    }else if(text4.length == 0){
-      nameError.style.display="block";
-    }
-  }
-                           
-  })
-
 
 
 
