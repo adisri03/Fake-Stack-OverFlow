ba48f24955b711c56692fc43bc95389c48073e8c
Adisri Rajkumar <54918307+adisri03@users.noreply.github.com>
2023-09-19 14:18:28 -0400

    Update index.js
    js for error messages

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ window.onload = function() {
   const form = document.getElementById('form');
   const container = document.getElementById('top');
 
+
   form.style.display = 'none';
   container.style.display = 'block';
 
@@ -26,10 +27,33 @@ window.onload = function() {
 });
 
   formBtn.addEventListener('click', () => {
-    if (form.checkValidity() === false) {
-      window.alert("Please fill out all required fields. Please, dummy. Yea you.");
-      
+    const title = document.getElementById('questionTitle');
+    const text = document.getElementById('questionText');
+    const tag = document.getElementById('tagText');
+    const name = document.getElementById('userName');
+
+    const titleError = document.getElementById('titleError');
+    const textError = document.getElementById('textError');
+    const tagError = document.getElementById('tagError');
+    const nameError = document.getElementById('nameError');
+
+    const text1 = title.value;
+    const text2 = text.value;
+    const text3 = tag.value;
+    const text4 = name.value
+
+    if (text1.length == 0 || text1.length > 100) {
+    // Display a message indicating the character count is valid
+      titleError.style.display="block";
+    }else if(text2.length == 0){
+      textError.style.display="block";
+    }else if(text3.length == 0){
+     tagError.style.display="block";
+    }else if(text4.length == 0){
+      nameError.style.display="block";
     }
+  }
+                           
   })
 
 
